91a1bfdfe9854062a5bd4866bfd94a67c505f6a1
Rahul Butani <rr.butani@gmail.com>
2020-02-22 20:22:03 -0600

    tui: some structure

--- src/tui.rs
+++ src/tui.rs
@@ -1,6 +1,7 @@
 //! TODO
 
-use lc3_shims::peripherals::SourceShim;
+use lc3_shims::peripherals::{Source, SourceShim};
+use lc3_shims::peripherals::Sink;
 use lc3_traits::peripherals::PeripheralSet;
 use lc3_shims::peripherals::{GpioShim, AdcShim, PwmShim, TimersShim, ClockShim, InputShim, OutputShim};
 use lc3_traits::control::rpc::{EventFuture, SyncEventFutureSharedState};
@@ -13,17 +14,15 @@ pub trait InputSink {
     // Note: probably only ASCII for now.
     //
     // Should return `None` on errors/invalid chars.
-    fn put_char(&mut self, c: char) -> Option<()>;
+    fn put_char(&self, c: char) -> Option<()>;
 }
 
 pub trait OutputSource {
     // Note: probably only ASCII for now.
     //
     // Should return `None` when no characters are available.
-    fn get_chars(&mut self) -> Option<String>;
+    fn get_chars(&self) -> Option<String>;
 }
-
-// TODO: impl the above for the Input and Output Shim.
 
 pub(crate) struct Shims<'a> {
     gpio: Arc<RwLock<GpioShim<'a>>>,
@@ -46,7 +45,7 @@ type ShimPeripheralSet<'a> = PeripheralSet<
 
 // This is fine!
 impl InputSink for &SourceShim {
-    fn put_char(&mut self, c: char) -> Option<()> {
+    fn put_char(&self, c: char) -> Option<()> {
         self.push(c);
         Some(())
     }
@@ -55,9 +54,9 @@ impl InputSink for &SourceShim {
 // This is less fine.. (should maybe be as generic as the Sink trait, but that
 // is not trivial) (TODO)
 impl OutputSource for Box<Mutex<Vec<u8>>> {
-    fn get_chars(&mut self) -> Option<String> {
+    fn get_chars(&self) -> Option<String> {
         // This is bad, maybe:
-        let vec = self.lock().unwrap();
+        let mut vec = self.lock().unwrap();
         if !vec.is_empty() {
             let v = std::mem::replace(vec.deref_mut(), Vec::new());
 
@@ -72,30 +71,28 @@ impl OutputSource for Box<Mutex<Vec<u8>>> {
 
 // TODO: move this to the right place...
 // fn new_shim_peripherals_set<'a>() -> (ShimPeripheralSet<'a>, &'static SourceShim, &'static Mutex<Vec<u8>>) { // TODO: char instead of u8...
-fn new_shim_peripherals_set<'a>() -> (ShimPeripheralSet<'a>, &'static SourceShim, &'static Mutex<Vec<u8>>) { // TODO: char instead of u8...
+fn new_shim_peripherals_set<'a, I, O>(input: &'a I, output: &'a O) -> (ShimPeripheralSet<'a>, &'a impl InputSink, &'a impl OutputSource)
+where
+    I: InputSink + Source + Send + Sync + 'a,
+    O: OutputSource + Sink + Send + Sync + 'a,
+{
     let gpio_shim = Arc::new(RwLock::new(GpioShim::default()));
     let adc_shim = Arc::new(RwLock::new(AdcShim::default()));
     let pwm_shim = Arc::new(RwLock::new(PwmShim::default()));
     let timer_shim = Arc::new(RwLock::new(TimersShim::default()));
     let clock_shim = Arc::new(RwLock::new(ClockShim::default()));
 
-    let source_shim = Box::new(SourceShim::new());
-    let source_shim = Box::leak(source_shim); // TODO: don't do this!!!!!
-    let input_shim = Arc::new(Mutex::new(InputShim::with_ref(source_shim)));
-
-    // TODO: use a ring buffer or something instead of a Vec for the Sink impl.
-    let console_output = Box::new(Mutex::new(Vec::new()));
-    let console_output = Box::leak(console_output); // TODO: don't do this!!!
-    let output_shim = Arc::new(Mutex::new(OutputShim::with_ref(console_output)));
+    let input_shim = Arc::new(Mutex::new(InputShim::with_ref(input)));
+    let output_shim = Arc::new(Mutex::new(OutputShim::with_ref(output)));
 
     (PeripheralSet::new(gpio_shim, adc_shim, pwm_shim, timer_shim, clock_shim, input_shim, output_shim),
-        source_shim,
-        console_output,
+        input,
+        output,
     )
 }
 
 impl<'a> Shims<'a> {
-    fn from_peripheral_set(p: &ShimPeripheralSet<'a>) -> Self {
+    pub(crate) fn from_peripheral_set(p: &ShimPeripheralSet<'a>) -> Self {
         Self {
             gpio: p.gpio.clone(),
             adc: p.adc.clone(),
@@ -106,15 +103,15 @@ impl<'a> Shims<'a> {
     }
 }
 
-pub struct Tui<'a, C: Control + 'a, I: InputSink + 'a, O: OutputSource + 'a> {
-    sim: C,
-    input: I,
-    output: O,
+pub struct Tui<'a, C: Control + ?Sized + 'a, I: InputSink + ?Sized + 'a, O: OutputSource + ?Sized + 'a> {
+    sim: &'a mut C,
+    input: &'a I,
+    output: &'a O,
     shims: Option<Shims<'a>>,
 }
 
-impl<'a, C: Control + 'a, I: InputSink + 'a, O: OutputSource + 'a> Tui<'a, C, I, O> {
-    pub fn new(sim: C, input: I, output: O) -> Self {
+impl<'a, C: Control + ?Sized + 'a, I: InputSink + ?Sized + 'a, O: OutputSource + ?Sized + 'a> Tui<'a, C, I, O> {
+    pub fn new(sim: &'a mut C, input: &'a I, output: &'a O) -> Self {
         Self {
             sim,
             input,
@@ -123,25 +120,46 @@ impl<'a, C: Control + 'a, I: InputSink + 'a, O: OutputSource + 'a> Tui<'a, C, I,
         }
     }
 
-    pub fn new_with_shims(sim: C, shims: &ShimPeripheralSet<'a>) -> Self {
-        Self {
-            sim,
-
-            shims: Some()
-        }
+    // The concern with offerring this interface instead of the below is that
+    // it's more likely users could attach a set of shim peripherals that aren't
+    // actually being used with the simulator.
+    //
+    // But, in reality, I think this is equally likely either way and this is a
+    // nicer API.
+    pub fn attach_shims(mut self, shims: &'a ShimPeripheralSet<'a>) -> Self {
+        self.shims = Some(Shims::from_peripheral_set(shims));
+        self
     }
 
-    pub fn run(self) -> Result<(), failure::Error> {
-        loop {
+    // pub fn new_with_shims(sim: C, input: &'a I, output: &'a O, shims: &'a ShimPeripheralSet<'a>) -> Self {
+    //     Self {
+    //         sim,
+    //         input,
+    //         output,
+    //         shims: Some(Shims::from_peripheral_set(shims))
+    //     }
+    // }
 
-        }
-    }
+    // pub fn run(self) -> Result<(), failure::Error> {
+    //     loop {
+
+    //     }
+    // }
 }
 
-type BoxedControl<'a> = Box<(dyn Control<EventFuture = EventFuture<'a, SyncEventFutureSharedState>> + 'a)>;
-// type BoxedInputSink<'a> = Box<(dyn Control<EventFuture = EventFuture<'a, SyncEventFutureSharedState>> + 'a)>;
-// type BoxedOutputSink<'a> =
+type DynControl<'a> = (dyn Control<EventFuture = EventFuture<'a, SyncEventFutureSharedState>> + 'a);
+type DynInputSink<'a> = (dyn InputSink + 'a);
+type DynOutputSource<'a> = (dyn OutputSource + 'a);
 
-// impl Tui<Box<dyn Control<EventFuture = EventFuture<SyncEventFutureSharedState>>>, Box<dyn InputSink>, Box<dyn OutputSource>> {
+pub type DynTui<'a> = Tui<'a, DynControl<'a>, DynInputSink<'a>, DynOutputSource<'a>>;
 
-// }
+impl<'a> DynTui<'a> {
+    pub fn new_boxed<C, I, O>(sim: &'a mut C, input: &'a I, output: &'a O) -> Self
+    where
+        C: Control<EventFuture = EventFuture<'a, SyncEventFutureSharedState>> + 'a,
+        I: InputSink + 'a,
+        O: OutputSource + 'a,
+    {
+        Self::new(sim, input, output)
+    }
+}
