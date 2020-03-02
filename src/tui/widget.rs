8b2deda122cc93b7943fe501c4a1e3dba110546f
Rahul Butani <rr.butani@gmail.com>
2020-03-01 23:10:24 -0600

    tui: widget clean up

--- src/tui/widget.rs
+++ src/tui/widget.rs
@@ -16,34 +16,12 @@ use tui::layout::Rect;
 
 use std::marker::PhantomData;
 
-// pub trait Widget: TuiWidget {
-//     /// For functions that don't hold their own state and need a reference to
-//     /// the [`Control`] impl to redraw themselves.
-//     ///
-//     /// By default, this just ignores the [`Control`] entirely and just calls
-//     /// the regular draw function on
-//     /// [the `tui` `Widget` trait](tui::widgets::Widget). Functions that don't
-//     /// need a [`Control`] instance need not override the default impl.
-//     ///
-//     /// [`Control`]: `lc3_traits::control::Control`
-//     fn draw<C: Control, B: Backend>(&mut self, _sim: &C, f: &mut Frame<'_, B>, area: Rect) {
-//         self.render(f, area)
-//     }
-
-//     fn update<'a, 'int, C, I, O>(&mut self, event: WidgetEvent, data: &mut TuiData<'a, 'int, C, I, O>)
-//     where
-//         C: Control + ?Sized + 'a,
-//         I: InputSink + ?Sized + 'a,
-//         O: OutputSource + ?Sized + 'a;
-// }
-
 pub trait Widget<'a, 'int, C, I, O, B>: TuiWidget
 where
     C: Control + ?Sized + 'a,
     I: InputSink + ?Sized + 'a,
     O: OutputSource + ?Sized + 'a,
     B: Backend,
-    // for<'s> FakeWidget<'s, 'a, 'int, C, I, O, B, Self>: Sized
 {
     /// For functions that don't hold their own state and need a reference to
     /// the [`Control`] impl to redraw themselves.
@@ -58,13 +36,7 @@ where
         TuiWidget::draw(self, rect, buffer)
     }
 
-    // fn render(&mut self, _sim: &C, f: &mut Frame<'_, B>, area: Rect) where Self: Sized {
-    //     TuiWidget::render(self, f, area)
-    // }
-
     fn render<'s>(&'s mut self, sim: &'s C, f: &mut Frame<'_, B>, area: Rect) {
-        // TuiWidget::render(self, f, area)
-
         // This is tricky.
         //
         // We can't just call render on ourself because we can't guarantee that
@@ -73,13 +45,12 @@ where
         //
         // What we do is pass ourselves into a wrapper widget that is Sized.
         // We exploit the fact that `TuiWidget::render` goes and passes
-        // `TuiWidget::draw(self, ...)` the buffer. (TODO, finish comment)
+        // `TuiWidget::draw(self, ...)` the buffer; our impl of `TuiWidget` on
+        // `FakeWidget` goes and passes this buffer to the wrapped widget's
+        // `TuiWidget::draw` function.
 
         let mut fw = FakeWidget::<'s, 'a, 'int, _, _, _, _, _>(sim, self, PhantomData);
         <FakeWidget<'s, 'a, 'int, _, _, _, _, _> as TuiWidget>::render::<B>(&mut fw, f, area);
-
-        // fw.rend(f, area)
-        // TuiWidget::render(&mut &mut fw, f, area)
     }
 
     // This would be an associated const or a function if we didn't need to care
@@ -128,9 +99,8 @@ where
 }
 
 
-// #[derive(Debug)]
 #[allow(explicit_outlives_requirements)]
-pub/*(in crate::tui)*/ struct Widgets<'a, 'int, C, I, O, B>
+pub struct Widgets<'a, 'int, C, I, O, B>
 where
     C: Control + ?Sized + 'a,
     I: InputSink + ?Sized + 'a,
@@ -227,7 +197,6 @@ where
 
 // This exists to circumvent the `Sized` requirement on `TuiWidget::render`.
 #[allow(explicit_outlives_requirements)]
-// struct FakeWidget<'s, 'a: 's, 'int: 's, C, I, O, B, W>
 struct FakeWidget<'s, 'a, 'int, C, I, O, B, W>
 (&'s C, &'s mut W, PhantomData<(&'a I, &'a O, B, &'int ())>)
 where
@@ -237,23 +206,7 @@ where
     B: Backend,
     W: Widget<'a, 'int, C, I, O, B> + ?Sized;
 
-// trait Rats: Sized {
-//     fn bar(&self) where Self: Sized {
-//         println!("yui");
-//     }
-// }
-
-// impl<'s, 'a, 'int, C, I, O, B, W> Rats for &'s mut FakeWidget<'s, 'a, 'int, C, I, O, B, W>
-// where
-//     C: Control + ?Sized + 'a,
-//     I: InputSink + ?Sized + 'a,
-//     O: OutputSource + ?Sized + 'a,
-//     B: Backend,
-//     W: Widget<'a, 'int, C, I, O, B>
-// {}
-
-// impl<'s, 'a: 's, 'int: 's, C, I, O, B, W> TuiWidget for FakeWidget<'s, 'a, 'int, C, I, O, B, W>
-impl<'s, 'a, 'int, C, I, O, B, W> TuiWidget for /*&'s mut */FakeWidget<'s, 'a, 'int, C, I, O, B, W>
+impl<'s, 'a, 'int, C, I, O, B, W> TuiWidget for FakeWidget<'s, 'a, 'int, C, I, O, B, W>
 where
     C: Control + ?Sized + 'a,
     I: InputSink + ?Sized + 'a,
@@ -278,19 +231,6 @@ where
         // {
         //     widget.draw(area, self.terminal.current_buffer_mut());
         // }
-    }
-}
-
-impl<'s, 'a, 'int, C, I, O, B, W> FakeWidget<'s, 'a, 'int, C, I, O, B, W>
-where
-    C: Control + ?Sized + 'a,
-    I: InputSink + ?Sized + 'a,
-    O: OutputSource + ?Sized + 'a,
-    B: Backend,
-    W: Widget<'a, 'int, C, I, O, B> + ?Sized
-{
-    fn rend(&mut self, f: &mut Frame<'_, B>, area: Rect) {
-        Self::render(self, f, area)
     }
 }
 
