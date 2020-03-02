751b325cd0de17a4946b32ae3eb8d3867c65d10a
Rahul Butani <rr.butani@gmail.com>
2020-03-02 13:23:08 -0600

    tui: add ways to specify a custom layout to Tui::run

--- src/tui/run.rs
+++ src/tui/run.rs
@@ -1,22 +1,25 @@
 //! TODO!
 
-use lc3_traits::control::Control;
-use lc3_application_support::io_peripherals::{InputSink, OutputSource};
-
 use super::Res as Result;
 use super::Tui;
-use super::events;
+use super::events::{self, Event, WidgetEvent, FocusEvent, CrosstermEvent};
+use super::widget::{Widget, Widgets};
+
+use lc3_traits::control::Control;
+use lc3_application_support::event_loop::Backoff;
+use lc3_application_support::io_peripherals::{InputSink, OutputSource};
 
 use crossterm::{ExecutableCommand, execute};
 use crossterm::terminal::EnterAlternateScreen;
+use crossterm::event::{KeyEvent, KeyCode, KeyModifiers};
+use failure::err_msg;
 use tui::terminal::Terminal;
 use tui::backend::{Backend, CrosstermBackend};
 
-use std::io::Write;
+use std::io::{Stdout, Write};
 
 impl<'a, 'int, C: Control + ?Sized + 'a, I: InputSink + ?Sized + 'a, O: OutputSource + ?Sized + 'a> Tui<'a, 'int, C, I, O> {
-    // TODO: not sure if this is worth doing
-    pub fn run<B: Backend>(self, term: &mut Terminal<B>) -> Result<()>
+    pub fn run_with_custom_layout<B: Backend>(mut self, term: &mut Terminal<B>, mut root: Widgets<'a, 'int, C, I, O, B>) -> Result<()>
     where
         B: ExecutableCommand<&'static str>
     {
@@ -25,7 +28,8 @@ impl<'a, 'int, C: Control + ?Sized + 'a, I: InputSink + ?Sized + 'a, O: OutputSo
         todo!()
     }
 
-    pub fn run_with_crossterm(self) -> Result<()> {
+    // Run with crossterm; with or without your own special layout.
+    pub fn run_with_crossterm(self, root_widget: Option<Widgets<'a, 'int, C, I, O, CrosstermBackend<Stdout>>>) -> Result<()> {
         crossterm::terminal::enable_raw_mode()?;
 
         let mut stdout = std::io::stdout();
@@ -36,7 +40,10 @@ impl<'a, 'int, C: Control + ?Sized + 'a, I: InputSink + ?Sized + 'a, O: OutputSo
 
         terminal.hide_cursor()?;
 
-        // self.run_with_crossterm(&mut terminal)
-        self.run(&mut terminal)
+        if let Some(w) = root_widget {
+            self.run_with_custom_layout(&mut terminal, w)
+        } else {
+            self.run(&mut terminal)
+        }
     }
 }
