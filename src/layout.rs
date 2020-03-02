08190e052d45d2a4ff67965e092be9adb8c96174
Rahul Butani <rr.butani@gmail.com>
2020-03-02 14:58:08 -0600

    tui: add a sample layout to play with
    Had to add some new lifetime bounds.. these don't make much sense (I think 'a is getting tied to the lifetime of the function?) and seem like they'll cause trouble but it works for now™.

--- src/layout.rs
+++ src/layout.rs
@@ -17,14 +17,34 @@ use tui::style::{Style, Color};
 // This is currently 'static' (i.e. doesn't change based on the inputs given)
 // but that could change in the future.
 // TODO: potentially parameterize this from with user configurable options!
-pub fn layout<'a, 'int, C, I, O, B>() -> Widgets<'a, 'int, C, I, O, B>
+pub fn layout<'a, 'int: 'a, C, I, O, B: 'a>() -> Widgets<'a, 'int, C, I, O, B>
 where
     C: Control + ?Sized + 'a,
     I: InputSink + ?Sized + 'a,
     O: OutputSource + ?Sized + 'a,
     B: Backend,
 {
-    let root = Widgets::new(Layout::default().direction(Direction::Vertical));
+    let horz = Layout::default().direction(Direction::Horizontal);
+    let vert = Layout::default().direction(Direction::Vertical);
+    let b = Block::default()
+        .title_style(Style::default().fg(Color::Red))
+        .borders(Borders::ALL)
+        .border_style(Style::default().fg(Color::White))
+        .style(Style::default().bg(Color::Reset));
+
+    let mut root = Widgets::<'a, 'int, C, I, O, B>::new(horz.clone());
+
+    let mut left = Widgets::<'a, 'int, C, I, O, B>::new(vert.clone());
+    let _ = left.add_widget(Constraint::Percentage(50), Empty, Some(b.clone().title("Top Left")))
+        .add_widget(Constraint::Percentage(50), Empty, Some(b.clone().title("Bottom Left")));
+
+    let mut right = Widgets::new(vert.clone());
+    let _ = right.add_widget(Constraint::Percentage(50), Empty, Some(b.clone().border_style(Style::default().fg(Color::Blue)).title("Top Right")))
+        .add_widget(Constraint::Percentage(50), Empty, Some(b.clone().title("Bottom Right")));
+
+    let _ = root.add_widget(Constraint::Percentage(40), left, None)
+        .add_widget(Constraint::Percentage(20), Empty, None)
+        .add_widget(Constraint::Percentage(40), right, None);
 
     root
 }
