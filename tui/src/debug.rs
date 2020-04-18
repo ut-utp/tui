564b2f27b190ec10f4458c1d8ebde098631f166c
Rahul Butani <rr.butani@gmail.com>
2020-04-18 00:33:34 -0500

    tui: have the event threads terminate gracefully

--- tui/src/debug.rs
+++ tui/src/debug.rs
@@ -5,3 +5,11 @@ pub const DEBUG_ENV_VAR: &'static str = "TUI_DEBUG";
 pub fn in_debug_mode() -> bool {
     std::env::var_os(DEBUG_ENV_VAR).is_some()
 }
+
+pub fn run_if_debugging<R, F: FnOnce() -> R>(func: F) -> Option<R> {
+    if in_debug_mode() {
+        Some((func)())
+    } else {
+        None
+    }
+}
