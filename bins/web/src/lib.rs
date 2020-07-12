7a437537406cd0f2dc660801fe80824c595a6b49
Rahul Butani <rr.butani@gmail.com>
2020-07-12 14:50:38 -0700

    web: first step towards drag and drop for files

--- bins/web/src/lib.rs
+++ bins/web/src/lib.rs
@@ -6,12 +6,57 @@ use lc3_application_support::init::{BlackBox, SimDevice};
 
 use console_error_panic_hook::set_once as set_panic_hook;
 use log::Level;
-use wasm_bindgen::prelude::*;
-use web_sys::Url;
+use wasm_bindgen::{prelude::*, JsCast};
+use web_sys::{Document, DragEvent, Event, HtmlElement, Url};
 use xterm_js_sys::xterm::{LogLevel, Terminal, TerminalOptions, Theme};
 
 use std::time::Duration;
 use std::str::FromStr;
+
+// Note: this leaks which we'll say is okay since these closures are used for
+// the entire life of the program anyways.
+pub fn register_drag_hooks(document: &Document) -> Result<(), JsValue> {
+    let drop_div = document
+        .get_element_by_id("drop")
+        .expect("should have a drop div")
+        .dyn_into::<HtmlElement>()?;
+
+    let enter = Closure::wrap(Box::new({ let div = drop_div.clone(); move |ev: DragEvent| {
+        AsRef::<Event>::as_ref(&ev).prevent_default();
+
+        // TODO: get the file name and make a message here!
+
+        log::error!("enter");
+
+        div.set_class_name("hover");
+    }}) as Box<dyn FnMut(_)>);
+
+    let exit = Closure::wrap(Box::new({ let div = drop_div.clone(); move |ev: DragEvent| {
+        AsRef::<Event>::as_ref(&ev).prevent_default();
+
+        log::error!("exit");
+        div.set_class_name("");
+    }}) as Box<dyn FnMut(_)>);
+
+    let drop = Closure::wrap(Box::new({ let div = drop_div.clone(); move |ev: DragEvent| {
+        AsRef::<Event>::as_ref(&ev).prevent_default();
+
+        log::error!("drop");
+        div.set_class_name("dropped");
+
+        // TODO: load the file, update the URL, reload the page.
+    }}) as Box<dyn FnMut(_)>);
+
+    drop_div.set_ondragenter(Some(enter.as_ref().unchecked_ref()));
+    drop_div.set_ondragexit(Some(exit.as_ref().unchecked_ref()));
+    drop_div.set_ondrop(Some(drop.as_ref().unchecked_ref()));
+
+    enter.forget();
+    exit.forget();
+    drop.forget();
+
+    Ok(())
+}
 
 #[wasm_bindgen]
 pub async fn run() -> Result<(), JsValue> {
@@ -36,6 +81,8 @@ pub async fn run() -> Result<(), JsValue> {
 
     term.open(terminal_div);
     term.resize(200, 45);
+
+    register_drag_hooks(&document)?;
 
     let mut b = BlackBox::new();
     let mut tui = DynTui::new_boxed_from_init::<SimDevice>(&mut b);
