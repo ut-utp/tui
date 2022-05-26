a92c430bc67cd8923679d50dd538022a9e948373
Rahul Butani <rrbutani@users.noreply.github.com>
2022-05-26 17:44:05 -0500

    misc: add nix flake, pin the toolchain

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "UTP TUI Flake";
+
+  inputs = {
+    nixpkgs.url      = github:NixOS/nixpkgs/nixos-21.11;
+    rust-overlay.url = github:oxalica/rust-overlay;
+    flake-utils.url  = github:numtide/flake-utils;
+  };
+
+  # TODO: cargo extensions
+  # TODO: wasm things (node, etc.)
+  # TODO: CI setup? (garnix)
+  # TODO: expose app targets, etc.
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+          ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+            darwin.apple_sdk.frameworks.Security
+          ];
+          shellHook = ''
+          '';
+        };
+      }
+    );
+}
